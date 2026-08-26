f0cfe27aa7a7855544d80be2f9feb22239d9925f
lumamaria <luma.maria@escolar.ifrn.edu.br>
2026-08-26 15:02:42 -0300

    Oficial

--- projeto_aluno/alunos/models.py
+++ projeto_aluno/alunos/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-# Create your models here.
-class Aluno(models.Model):
-   nome = models.CharField(max_length=100)
-   idade = models.IntegerField()
-   email = models.EmailField()
-   created_at = models.DateTimeField(auto_now_add=True)
-   updated_at = models.DateTimeField(auto_now=True)
 
-   def __str__(self):
-       return self.nome
+class Aluno(models.Model):
+    matricula = models.CharField(max_length=20)
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    curso = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+    
--- projeto_aluno/alunos/urls.py
+++ projeto_aluno/alunos/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'alunos'
 
 urlpatterns = [
-    path('', views.alunos_list, name='alunos-list'),
-    path('<int:id>/', views.aluno_detalhe, name='aluno-detalhe'),
+    path('', views.alunos_list, name='alunos_list'),
+    path('<int:id>/', views.aluno_detalhe, name='aluno_detalhe'),
 ]
